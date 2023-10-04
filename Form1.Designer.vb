765ac26e0f7747a1ea65a25c7939873db3ee80b9
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 01:14:48 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        btnOk = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(317, 34)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "Visual"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(317, 84)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 0
+        TextBox2.Text = "Studio"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(317, 143)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 0
+        TextBox3.Text = "2022"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(317, 205)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 0
+        ' 
+        ' btnOk
+        ' 
+        btnOk.Location = New Point(243, 266)
+        btnOk.Name = "btnOk"
+        btnOk.Size = New Size(75, 23)
+        btnOk.TabIndex = 1
+        btnOk.Text = "Ok"
+        btnOk.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(190, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(34, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Text1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(190, 92)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(34, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Text2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(190, 151)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(34, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Text3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(190, 213)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(34, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "Text4"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDarkDark
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnOk)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+
+        Dim Text1, Text2, Text3, Text4 As String
+        Text1 = "Visual"
+        Text2 = "Studio"
+        Text3 = "2022"
+        Text4 = (TextBox1.Text) & (TextBox2.Text) & (TextBox3.Text)
+        MsgBox(Text4)
+
+
+
+    End Sub
+End Class
